1f37e7c476113b1213e25d52502b5b1d073135fa
johnnytortorici <johnny.tortorici@gmail.com>
2020-06-17 20:43:40 -0400

    Exercise 2.1-2.3

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -2,9 +2,28 @@
 const FROGS = 3;
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
+const ol = document.querySelector('#track');
 
-// 2. Create li
+for (let i = 1; i <= FROGS; i++) {
+    // 2. Create li
+    const lane = document.createElement('li');
+    // 3. Create span and add it to the li
+    lane.innerHTML = `<span>${i}</span>`;
+    // 4. Assign an id to each lane
+    lane.id = `lane-${i}`;
+    ol.appendChild(lane);
+}
 
-// 3. Create span and add it to the li
+// Define racers array
+let racers = [];
 
-// 4. Assign an id to each lane
+for (let i = 0; i < FROGS; i++) {
+    racers[i] = frogStable[i];
+    racers[i].progress = 0;
+    racers[i].lane = i+1;
+    let lane = document.querySelector(`#lane-${i+1}`);
+    lane.innerHTML = `<span class='frog' style='background-color: ${racers[i].color}'>${racers[i].name} ${racers[i].number} ${racers[i].progress}</span>`;
+    
+}
+
+console.log(racers);
